7430cafc87e814e3516c7604752f3acb9ce0a807
Nick Miano <nmiano84@gmail.com>
2014-08-10 04:04:39 -0400

    cost func

--- p5/linearRegCostFunction.m
+++ p5/linearRegCostFunction.m
@@ -1,37 +1,29 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
-%regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
-%   data points in X and y. Returns the cost in J and the gradient in grad
+	%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+	%regression with multiple variables
+	%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+	%   cost of using theta as the parameter for linear regression to fit the 
+	%   data points in X and y. Returns the cost in J and the gradient in grad
 
-% Initialize some useful values
-m = length(y); % number of training examples
+	% Initialize some useful values
+	m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+	% You need to return the following variables correctly 
+	J = 0;
+	grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+	% ====================== YOUR CODE HERE ======================
+	% Instructions: Compute the cost and gradient of regularized linear 
+	%               regression for a particular choice of theta.
+	%
+	%               You should set J to the cost and grad to the gradient.
+	%
+	twoM = 2*m;
+	hMinusY = (X*theta) - y;
+	subTheta = theta(2:end,:)
+	J = (1/twoM)*(hMinusY'*hMinusY) + ((lambda/twoM)*sum(subTheta.*subTheta));
+	% =========================================================================
 
-
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
-grad = grad(:);
+	grad = grad(:);
 
 end
